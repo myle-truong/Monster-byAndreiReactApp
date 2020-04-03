2ce0ede75d91a7acad87e72f0e10ec6e394ea28b
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-04-03 22:06:18 +0200

    Updates

---
+++ precache-manifest.dd5285ca78039be1ce1f2bbacab78486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828dff2ff167502b7e48c48e501e6666",
+    "url": "/Monster-byAndreiReactApp/index.html"
+  },
+  {
+    "revision": "f841eb4fd19347baf11d",
+    "url": "/Monster-byAndreiReactApp/static/css/main.47b70904.chunk.css"
+  },
+  {
+    "revision": "696cd664e94359881443",
+    "url": "/Monster-byAndreiReactApp/static/js/2.259342ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-byAndreiReactApp/static/js/2.259342ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f841eb4fd19347baf11d",
+    "url": "/Monster-byAndreiReactApp/static/js/main.b0f1e955.chunk.js"
+  },
+  {
+    "revision": "a657bb27353e8e2f131d",
+    "url": "/Monster-byAndreiReactApp/static/js/runtime-main.b656c936.js"
+  }
+]);
